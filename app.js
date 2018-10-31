a01fbd121f8fe795927db811b434d62a2ff34cc6
igorkhorev <igorhorev@gmail.com>
2018-10-31 16:39:49 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let indexRouter = require('./routes/index');
+// let usersRouter = require('./routes/users');
+// let birdsRouter = require('./routes/birds');
+
+let mapperRouter = require('./routes/mapper');
+
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+app.use('/mapper', mapperRouter);
+
+// app.use('/users', usersRouter);
+// app.use('/birds', birdsRouter);
+
+module.exports = app;
